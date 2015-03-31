78b4a912383ecfeaae23cb0cbba2880c71c459c3
Tone Hoeft <tone@tonehoeft.com>
2015-03-31 06:43:32 +0000

    store JSON locally to fix CORS issue

--- js/services/emails.js
+++ js/services/emails.js
@@ -1,5 +1,5 @@
 app.factory('emails', ['$http', function($http) {
-	return $http.get('http://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/emails.json')
+	return $http.get('emails.json')
 		.success(function(data) {
 			return data;
 		})
